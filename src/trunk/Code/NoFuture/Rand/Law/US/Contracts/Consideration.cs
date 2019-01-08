f83f9bd0f8e6a98c8b1e96d8aa3cf7bf35deb4da
NoFuture <brian.starrett@gmail.com>
2019-01-08 16:34:11 -0600

    more unit tests in Nf.Rand.Law

--- src/trunk/Code/NoFuture/Rand/Law/US/Contracts/Consideration.cs
+++ src/trunk/Code/NoFuture/Rand/Law/US/Contracts/Consideration.cs
@@ -4,20 +4,11 @@ using NoFuture.Rand.Law.Attributes;
 namespace NoFuture.Rand.Law.US.Contracts
 {
     /// <summary>
-    /// The type which from the reciprocal of an offer
+    /// Is intended to determine what is an enforcable promise and what is a donative one.
     /// </summary>
     /// <typeparam name="T"></typeparam>
-    /// <remarks>
-    /// <![CDATA[
-    /// - origin from "quid pro quo",
-    /// is it offered in order to get something in return,
-    /// something that has value in the eyes of the law.  
-    /// Illusionary promise: words in promissory form that promise nothing
-    /// ]]>
-    /// </remarks>
-    [Note("is performance or return promise bargained for", 
-          "is not just a choice (illusionary promise)",
-          "is not already obligated to do")]
+    [Note("is performance or return promise bargained for")]
+    [Aka("quid pro quo", "this for that")]
     public class Consideration<T> : ObjectiveLegalConcept where T : ObjectiveLegalConcept
     {
         private readonly LegalContract<T> _contract;
@@ -39,6 +30,28 @@ namespace NoFuture.Rand.Law.US.Contracts
         /// </summary>
         public virtual Func<ILegalPerson, ObjectiveLegalConcept, bool> IsGivenByPromisee { get; set; }
 
+        /// <summary>
+        /// What is bargained for must have value
+        /// </summary>
+        public virtual Predicate<T> IsValueInEyesOfLaw { get; set; } = o => true;
+
+        /// <summary>
+        /// What is bargined for must not be a choice - it must be a duty
+        /// </summary>
+        [Note("is not just a choice")]
+        public virtual Predicate<T> IsIllusionaryPromise { get; set; } = o => false;
+
+        /// <summary>
+        /// What is bargined for must not be an existing duty.  When it 
+        /// is an existing duty there is no consideration and whatever the other side
+        /// is bringing is a gift.
+        /// </summary>
+        /// <remarks>
+        /// <![CDATA[ e.g. cannot bargain that we promise not to steal from each other]]>
+        /// </remarks>
+        [Note("is not already obligated to be done")]
+        public virtual Predicate<T> IsExistingDuty { get; set; } = o => false;
+
         public override bool IsValid(ILegalPerson offeror, ILegalPerson offeree)
         {
             if (IsSoughtByPromisor == null)
@@ -53,7 +66,8 @@ namespace NoFuture.Rand.Law.US.Contracts
                 return false;
             }
 
-            var returnPromise = _contract.Acceptance(_contract.Offer);
+            var promise = _contract.Offer;
+            var returnPromise = _contract.Acceptance(promise);
             if (returnPromise == null)
             {
                 AddAuditEntry($"{nameof(returnPromise)} is null");
@@ -66,11 +80,50 @@ namespace NoFuture.Rand.Law.US.Contracts
                 return false;
             }
 
-            if (!IsGivenByPromisee(offeree, _contract.Offer))
+            if (!IsGivenByPromisee(offeree, promise))
             {
                 AddAuditEntry($"the offer is not what the {offeree.Name} wants");
                 return false;
             }
+
+            var valPredicate = IsValueInEyesOfLaw ?? (o => true);
+
+            if (!valPredicate(promise as T))
+            {
+                AddAuditEntry($"The promise given by {offeror.Name} has no value in the eyes-of-the-law.");
+                return false;
+            }
+
+            if (!valPredicate(returnPromise))
+            {
+                AddAuditEntry($"The return promise given by {offeree.Name} has no value in the eyes-of-the-law.");
+                return false;
+            }
+
+            var illusionPredicate = IsIllusionaryPromise ?? (o => false);
+            if (illusionPredicate(promise as T))
+            {
+                AddAuditEntry($"The promise given by {offeror.Name} is illusionary - it is not a promise at all.");
+                return false;
+            }
+            if (illusionPredicate(returnPromise))
+            {
+                AddAuditEntry($"The return promise given by {offeree.Name} is illusionary - it is not a promise at all.");
+                return false;
+            }
+
+            var existingDutyPredicate = IsExistingDuty ?? (o => false);
+            if (existingDutyPredicate(promise as T))
+            {
+                AddAuditEntry($"The promise given by {offeror.Name} is an existing duty and cannot be bargined with nor for.");
+                return false;
+            }
+            if (existingDutyPredicate(returnPromise))
+            {
+                AddAuditEntry($"The return promise given by {offeree.Name} is an existing duty and cannot be bargined with nor for.");
+                return false;
+            }
+
 
             return true;
         }
