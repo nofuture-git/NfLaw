8fdf598649c49b700c6499a9b2e41d4c1f4bb9b6
NoFuture <brian.starrett@gmail.com>
2019-01-14 15:12:28 -0600

    added another unit test to Nf.Rand.Law

--- src/trunk/Code/NoFuture/Rand/Law/US/Contracts/Defense/Voidable.cs
+++ src/trunk/Code/NoFuture/Rand/Law/US/Contracts/Defense/Voidable.cs
@@ -25,6 +25,11 @@ namespace NoFuture.Rand.Law.US.Contracts.Defense
         public virtual Predicate<ILegalPerson> IsMinor { get; set; }
 
         /// <summary>
+        /// A person considered by the court to be without the capacity to contract
+        /// </summary>
+        public virtual Predicate<ILegalPerson> IsMentallyIncompetent { get; set; }
+
+        /// <summary>
         /// <![CDATA[
         /// if the minor disaffirms the contract, the minor is not liable under it
         /// ]]>
@@ -67,6 +72,21 @@ namespace NoFuture.Rand.Law.US.Contracts.Defense
             {
                 AddReasonEntry("this defense is only applicable to valid contrax");
                 return false;
+            }
+
+            var isMental = IsMentallyIncompetent ?? (lp => false);
+            if (isMental(offeror))
+            {
+                AddReasonEntry($"the {nameof(offeror)} named {offeror.Name} is " +
+                               "a mentally incompetent");
+                return true;
+            }
+
+            if (isMental(offeree))
+            {
+                AddReasonEntry($"the {nameof(offeree)} named {offeree.Name} is " +
+                               "a mentally incompetent");
+                return true;
             }
 
             var isMinor = IsMinor ?? (lp => false);
