4c6671ad953527adeb00fc6e54544cddb7b6a151
NoFuture <brian.starrett@gmail.com>
2019-01-21 12:38:09 -0600

    implemented the objective test Nf.Rand.Law.Contracts.Defense.ToAssent.ByUndueInfluence and added unit test thereof

--- src/trunk/Code/NoFuture/Rand/Law/US/Contracts/Defense/ToAssent/ByUndueInfluence.cs
+++ src/trunk/Code/NoFuture/Rand/Law/US/Contracts/Defense/ToAssent/ByUndueInfluence.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using NoFuture.Rand.Law.Attributes;
 
 namespace NoFuture.Rand.Law.US.Contracts.Defense.ToAssent
 {
@@ -7,12 +9,61 @@ namespace NoFuture.Rand.Law.US.Contracts.Defense.ToAssent
     /// improper pressure to enter into a contract not the result of direct threats
     /// ]]>
     /// </summary>
-    /// <typeparam name="T"></typeparam>
+    /// <remarks>
+    /// ODORIZZI v. BLOOMFIELD SCHOOL DISTRICT 
+    /// Court of Appeal of California, Second Appellate District 
+    /// 246 Cal.App. 2d 123, 54 Cal.Rptr. 533 (2d Dist. 1966)    
+    /// </remarks>
+    [Aka("overpersuasion")]
     public class ByUndueInfluence<T> : DefenseBase<T>, IVoidable
     {
         public ByUndueInfluence(IContract<T> contract) : base(contract)
         {
         }
+
+        /// <summary>
+        /// <![CDATA[(1) discussion of the transaction at an unusual or inappropriate time,]]>
+        /// </summary>
+        public Predicate<ILegalPerson> IsUnusualTime { get; set; } = lp => false;
+
+        /// <summary>
+        /// <![CDATA[(2) consummation of the transaction in an unusual place,]]>
+        /// </summary>
+        public Predicate<ILegalPerson> IsUnusualLocation { get; set; } = lp => false;
+
+        /// <summary>
+        /// <![CDATA[(3) insistent demand that the business be finished at once]]>
+        /// </summary>
+        public Predicate<ILegalPerson> IsInsistentOnImmediateCompletion { get; set; } = lp => false;
+
+        /// <summary>
+        /// <![CDATA[(4) extreme emphasis on untoward consequences of delay,]]>
+        /// </summary>
+        public Predicate<ILegalPerson> IsExtremeEmphasisOnConsequencesOfDelay { get; set; } = lp => false;
+
+        /// <summary>
+        /// <![CDATA[(5) the use of multiple persuaders by the dominant side against a single servient party,]]>
+        /// </summary>
+        public Predicate<ILegalPerson> IsMultiPersuadersOnServientParty { get; set; } = lp => false;
+
+        /// <summary>
+        /// <![CDATA[(6) absence of third-party advisers to the servient party,]]>
+        /// </summary>
+        public Predicate<ILegalPerson> IsAbsentAdvisorsToServientParty { get; set; } = lp => false;
+
+        /// <summary>
+        /// <![CDATA[(7) statements that there is no time to consult financial advisers or attorneys]]>
+        /// </summary>
+        public Predicate<ILegalPerson> IsInsistentOnNoTime4Advisors { get; set; } = lp => false;
+
+        /// <summary>
+        /// <![CDATA[
+        /// If a number of these elements are simultaneously present, 
+        /// the persuasion may be characterized as excessive.
+        /// ]]>
+        /// </summary>
+        public int NumberNeededToTestTrue { get; set; } = 5;
+
         /// <summary>
         /// The objective test for the legal concept
         /// </summary>
@@ -33,9 +84,41 @@ namespace NoFuture.Rand.Law.US.Contracts.Defense.ToAssent
         {
             if (!base.IsValid(offeror, offeree))
                 return false;
+            var testTrueCount = 0;
+            foreach (var kvp in GetPredicate2ReasonString())
+            {
+                var p = kvp.Key;
+                if (p(offeror) || p(offeree))
+                {
+                    AddReasonEntry(kvp.Value);
+                    testTrueCount += 1;
+                }
+            }
 
+            if (testTrueCount >= NumberNeededToTestTrue)
+            {
+                AddReasonEntry("therefore, persuasion is excessive");
+                return true;
+            }
 
-            throw new NotImplementedException();
+            return false;
+
         }
+
+        protected internal virtual Dictionary<Predicate<ILegalPerson>, string> GetPredicate2ReasonString()
+        {
+            return new Dictionary<Predicate<ILegalPerson>, string>
+            {
+                {IsUnusualTime, "discussion of the transaction at an unusual or inappropriate time,"},
+                {IsUnusualLocation, "consummation of the transaction in an unusual place,"},
+                {IsInsistentOnImmediateCompletion, "insistent demand that the business be finished at once,"},
+                {IsExtremeEmphasisOnConsequencesOfDelay, "extreme emphasis on untoward consequences of delay,"},
+                {IsMultiPersuadersOnServientParty, "the use of multiple persuaders by the dominant side against a single servient party,"},
+                {IsAbsentAdvisorsToServientParty, "absence of third-party advisers to the servient party,"},
+                {IsInsistentOnNoTime4Advisors, "statements that there is no time to consult financial advisers or attorneys,"}
+            };
+        }
+
+
     }
 }
