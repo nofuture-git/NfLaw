90bcbf8f6d6f2e850600ad3035ed5b3699186d49
NoFuture <brian.starrett@gmail.com>
2019-01-24 16:48:07 -0600

    clean up in Nf.Rand.Law and unit tests

--- src/trunk/Code/NoFuture/Rand/Law/US/Contracts/Litigate/SemanticDilemma.cs
+++ src/trunk/Code/NoFuture/Rand/Law/US/Contracts/Litigate/SemanticDilemma.cs
@@ -6,9 +6,16 @@ namespace NoFuture.Rand.Law.US.Contracts.Litigate
 {
     /// <summary>
     /// A type to handle the problem of a court having to pick one of two
-    /// term meanings as the valid one.
+    /// term meanings as the intended one.
     /// </summary>
-    /// <typeparam name="T"></typeparam>
+    /// <remarks>
+    /// <![CDATA[
+    /// The ordinary standard, or "plain meaning," is simply the meaning of the people who did 
+    /// not write the document. The fallacy consists in assuming that there is or ever can be 
+    /// some one real or absolute meaning. In truth, there can be only some person's meaning; 
+    /// and that person, whose meaning the law is seeking, is the writer of the document.
+    /// ]]>
+    /// </remarks>
     public class SemanticDilemma<T> : LitigateBase<T>
     {
         public SemanticDilemma(IContract<T> contract) : base(contract)
@@ -21,9 +28,10 @@ namespace NoFuture.Rand.Law.US.Contracts.Litigate
         public Predicate<Term<object>> IsPrerequisite { get; set; } = t => true;
 
         /// <summary>
-        /// The method in which one and only one of the dilemma terms is decieded.
+        /// The method in which one and only one of the dilemma terms is the one 
+        /// the original parties intended.
         /// </summary>
-        public Predicate<Term<object>> IsPreferred { get; set; } = t => false;
+        public Predicate<Term<object>> IsIntendedMeaningAtTheTime { get; set; } = t => false;
 
         public override bool IsValid(ILegalPerson offeror, ILegalPerson offeree)
         {
@@ -63,8 +71,8 @@ namespace NoFuture.Rand.Law.US.Contracts.Litigate
                 if (!isSemanticConflict)
                     continue;
 
-                var isOfferorPreferred = IsPreferred(offerorTerm);
-                var isOffereePreferred = IsPreferred(offereeTerm);
+                var isOfferorPreferred = IsIntendedMeaningAtTheTime(offerorTerm);
+                var isOffereePreferred = IsIntendedMeaningAtTheTime(offereeTerm);
                 var isOneOnlyOneValid = isOfferorPreferred ^ isOffereePreferred;
                 if (!isOneOnlyOneValid)
                 {
