1aefdd71c062200b0259f1326d51fcce44ad037f
NoFuture <brian.starrett@gmail.com>
2019-02-01 14:35:43 -0600

    finished implementation of CoastalSteelvAlgernonTests

--- src/trunk/Code/NoFuture/Rand/Law/US/Contracts/Remedy/MoneyDmg/QuantumMeruitRestitution.cs
+++ src/trunk/Code/NoFuture/Rand/Law/US/Contracts/Remedy/MoneyDmg/QuantumMeruitRestitution.cs
@@ -6,6 +6,10 @@ namespace NoFuture.Rand.Law.US.Contracts.Remedy.MoneyDmg
     /// <summary>
     /// <![CDATA[Restatement (Second) of Contracts § 343(2) ]]>
     /// </summary>
+    /// <remarks>
+    /// the amount which the services could have been purchased from 
+    /// one in that position, time and circumstances.
+    /// </remarks>
     public class QuantumMeruitRestitution<T> : MoneyDmgBase<T> where T : IObjectiveLegalConcept
     {
         public QuantumMeruitRestitution(IContract<T> contract) : base(contract)
