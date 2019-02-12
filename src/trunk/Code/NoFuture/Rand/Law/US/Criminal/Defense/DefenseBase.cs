1d2038a9d3da3cd309a5fe903204c36243ace0a2
NoFuture <brian.starrett@gmail.com>
2019-02-12 09:49:02 -0600

    missing addition form last commit

---
+++ src/trunk/Code/NoFuture/Rand/Law/US/Criminal/Defense/DefenseBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoFuture.Rand.Law.US.Criminal.Defense
+{
+    public abstract class DefenseBase : LegalConcept
+    {
+        public ICrime Crime { get; }
+
+        protected DefenseBase(ICrime crime)
+        {
+            Crime = crime;
+        }
+    }
+}
