2b58a4c72f86b167b2066f9dbc8d19847caca06a
NoFuture <brian.starrett@gmail.com>
2019-02-25 12:42:23 -0600

    added more types in Rand.Law.Criminal.HominiLupus

---
+++ src/trunk/Code/NoFuture/Rand/Law/Criminal/HominiLupus/US/Elements/Assault.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NoFuture.Rand.Law.Attributes;
+using NoFuture.Rand.Law.Criminal.US;
+using NoFuture.Rand.Law.Criminal.US.Elements.Act;
+using NoFuture.Rand.Law.Criminal.US.Elements.Intent;
+
+namespace NoFuture.Rand.Law.Criminal.HominiLupus.US.Elements
+{
+    [Aka("attempted battery", "threatened battery")]
+    public class Assault : CriminalBase, IActusReus
+    {
+        public override bool IsValid(params ILegalPerson[] persons)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CompareTo(IMensRea criminalIntent, params ILegalPerson[] persons)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/trunk/Code/NoFuture/Rand/Law/Criminal/HominiLupus/US/Elements/Battery.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using NoFuture.Rand.Law.Criminal.US;
+using NoFuture.Rand.Law.Criminal.US.Elements.Act;
+using NoFuture.Rand.Law.Criminal.US.Elements.Intent;
+
+namespace NoFuture.Rand.Law.Criminal.HominiLupus.US.Elements
+{
+    /// <summary>
+    /// Same as <see cref="Assault"/> except there is actual touch\contact of some kind
+    /// </summary>
+    public class Battery : CriminalBase, IActusReus
+    {
+        public Predicate<ILegalPerson> IsPhysicalContact { get; set; } = lp => false;
+
+        /// <summary>
+        /// If the victim entered into Mutual Combat then its not battery
+        /// </summary>
+        public Consent Consent { get; set; }
+
+        public override bool IsValid(params ILegalPerson[] persons)
+        {
+            var defendant = GetDefendant(persons);
+            if (defendant == null)
+                return false;
+
+            if (!IsPhysicalContact(defendant))
+            {
+                AddReasonEntry($"defendant, {defendant.Name}, {nameof(IsPhysicalContact)} is false");
+            }
+
+            var isConsented = Consent?.IsValid(persons) ?? false;
+
+            AddReasonEntryRange(Consent?.GetReasonEntries());
+            return !isConsented;
+        }
+
+        public bool CompareTo(IMensRea criminalIntent, params ILegalPerson[] persons)
+        {
+            return true;
+        }
+    }
+}
