a122b57144239a20b04bac0698a41229a2b4288a
NoFuture <brian.starrett@gmail.com>
2019-03-04 13:35:28 -0600

    extracted a middle type between AgitPropertyBase and Burglary to represent CriminalTrespass

--- src/trunk/Code/NoFuture/Rand/Law/Criminal/AgainstProperty/US/Elements/Trespass/Burglary.cs
+++ src/trunk/Code/NoFuture/Rand/Law/Criminal/AgainstProperty/US/Elements/Trespass/Burglary.cs
@@ -1,18 +1,16 @@
 ﻿using System;
 using NoFuture.Rand.Law.Attributes;
-using NoFuture.Rand.Law.Criminal.AgainstProperty.US.Elements.Theft;
-using NoFuture.Rand.Law.Criminal.US.Elements.Act;
 using NoFuture.Rand.Law.Criminal.US.Elements.Intent;
 using NoFuture.Rand.Law.Criminal.US.Elements.Intent.ComLaw;
 using NoFuture.Rand.Law.Criminal.US.Elements.Intent.PenalCode;
 
-namespace NoFuture.Rand.Law.Criminal.AgainstProperty.US.Elements
+namespace NoFuture.Rand.Law.Criminal.AgainstProperty.US.Elements.Trespass
 {
     /// <summary>
     /// unlawful entry into almost any structure or vehicle at any time
     /// </summary>
     [Note("burg: german 'castle'", "baurgs: german 'city'")]
-    public class Burglary : AgitPropertyBase, IActusReus
+    public class Burglary : CriminalTrespass
     {
         public Predicate<ILegalProperty> IsStructuredEnclosure { get; set; } = lp => false;
 
@@ -21,18 +19,13 @@ namespace NoFuture.Rand.Law.Criminal.AgainstProperty.US.Elements
         /// </summary>
         public Predicate<ILegalPerson> IsBreakingForce { get; set; }
 
-        /// <summary>
-        /// partial or complete intrusion of either the defendant, the defendant&apos;s body part or a tool or instrument
-        /// </summary>
-        public Predicate<ILegalPerson> IsEntry { get; set; } = lp => false;
-
         public override bool IsValid(params ILegalPerson[] persons)
         {
-            var defendant = GetDefendant(persons);
-            if (defendant == null)
+            if (!base.IsValid(persons))
                 return false;
 
-            if (!GetConsent(persons))
+            var defendant = GetDefendant(persons);
+            if (defendant == null)
                 return false;
 
             if (IsBreakingForce != null && !IsBreakingForce(defendant))
@@ -47,23 +40,17 @@ namespace NoFuture.Rand.Law.Criminal.AgainstProperty.US.Elements
                 return false;
             }
 
-            if (!IsEntry(defendant))
-            {
-                AddReasonEntry($"defendant, {defendant.Name}, {nameof(IsEntry)} is false");
-                return false;
-            }
-
             return true;
         }
 
-        public virtual bool CompareTo(IMensRea criminalIntent, params ILegalPerson[] persons)
+        public override bool CompareTo(IMensRea criminalIntent, params ILegalPerson[] persons)
         {
             var validIntend = criminalIntent is Purposely || criminalIntent is SpecificIntent ||
                               criminalIntent is Knowingly || criminalIntent is GeneralIntent;
 
             if (!validIntend)
             {
-                AddReasonEntry($"{nameof(ByTaking)} requires intent " +
+                AddReasonEntry($"{nameof(Burglary)} requires intent " +
                                $"of {nameof(Purposely)}, {nameof(SpecificIntent)}, " +
                                $"{nameof(Knowingly)}, {nameof(GeneralIntent)}");
                 return false;
---
+++ src/trunk/Code/NoFuture/Rand/Law/Criminal/AgainstProperty/US/Elements/Trespass/CriminalTrespass.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using NoFuture.Rand.Law.Criminal.US.Elements.Act;
+using NoFuture.Rand.Law.Criminal.US.Elements.Intent;
+using NoFuture.Rand.Law.Criminal.US.Elements.Intent.ComLaw;
+using NoFuture.Rand.Law.Criminal.US.Elements.Intent.PenalCode;
+
+namespace NoFuture.Rand.Law.Criminal.AgainstProperty.US.Elements.Trespass
+{
+    public class CriminalTrespass : AgitPropertyBase, IActusReus
+    {
+        /// <summary>
+        /// partial or complete intrusion of either the defendant, the defendant&apos;s body part or a tool or instrument
+        /// </summary>
+        public Predicate<ILegalPerson> IsEntry { get; set; } = lp => false;
+
+        public override bool IsValid(params ILegalPerson[] persons)
+        {
+            var defendant = GetDefendant(persons);
+            if (defendant == null)
+                return false;
+
+            if (!GetConsent(persons))
+                return false;
+
+            if (!IsEntry(defendant))
+            {
+                AddReasonEntry($"defendant, {defendant.Name}, {nameof(IsEntry)} is false");
+                return false;
+            }
+
+            return true;
+        }
+
+        public virtual bool CompareTo(IMensRea criminalIntent, params ILegalPerson[] persons)
+        {
+            var validIntend = criminalIntent is Knowingly || criminalIntent is GeneralIntent;
+
+            if (!validIntend)
+            {
+                AddReasonEntry($"{nameof(CriminalTrespass)} requires intent " +
+                               $"{nameof(Knowingly)}, {nameof(GeneralIntent)}");
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
