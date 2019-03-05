9bdd7be506639bd27c7990b271b8be156ac4ebc4
NoFuture <brian.starrett@gmail.com>
2019-03-05 12:00:32 -0600

    added Loitering as another type to AgainstPublic assembly

---
+++ src/trunk/Code/NoFuture/Rand/Law/Criminal/AgainstPublic/US/Elements/Loitering.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NoFuture.Rand.Law.Attributes;
+using NoFuture.Rand.Law.Criminal.US;
+using NoFuture.Rand.Law.Criminal.US.Elements.Act;
+using NoFuture.Rand.Law.Criminal.US.Elements.Intent;
+using NoFuture.Rand.Law.Criminal.US.Elements.Intent.ComLaw;
+using NoFuture.Rand.Law.Criminal.US.Elements.Intent.PenalCode;
+
+namespace NoFuture.Rand.Law.Criminal.AgainstPublic.US.Elements
+{
+    /// <summary>
+    /// wandering or remaining at some location with intent to gamble, beg or prostitute
+    /// </summary>
+    public class Loitering : CriminalBase, IActusReus
+    {
+        [Aka("panhandling")] public Predicate<ILegalPerson> IsBegging { get; set; } = lp => false;
+
+        public Predicate<ILegalPerson> IsGambling { get; set; } = lp => false;
+
+        public Predicate<ILegalPerson> IsProstituting { get; set; } = lp => false;
+
+        //https://en.wikipedia.org/wiki/Loitering#United_States
+        //public ObjectivePredicate<ILegalPerson> IsGangGeographic { get; set; }
+
+        //public ObjectivePredicate<ILegalPerson> IsGangIntimidate { get; set; }
+
+        //public ObjectivePredicate<ILegalPerson> IsGangConcealment { get; set; }
+
+        public override bool IsValid(params ILegalPerson[] persons)
+        {
+            var defendant = GetDefendant(persons);
+            if (defendant == null)
+                return false;
+
+            var beg = IsBegging(defendant);
+            var gamble = IsGambling(defendant);
+            var prostitue = IsProstituting(defendant);
+
+            if (new[] {beg, gamble, prostitue}.All(p => p == false))
+            {
+                AddReasonEntry($"defendant, {defendant.Name}, {nameof(IsBegging)}, " +
+                               $"{nameof(IsGambling)} and {nameof(IsProstituting)} are false");
+                return false;
+            }
+
+            return true;
+        }
+
+        public bool CompareTo(IMensRea criminalIntent, params ILegalPerson[] persons)
+        {
+            var isvalid = criminalIntent is Purposely || criminalIntent is SpecificIntent;
+
+            if (!isvalid)
+            {
+                AddReasonEntry($"{nameof(Loitering)} requires intent " +
+                               $"of {nameof(Recklessly)}, {nameof(Purposely)}, " +
+                               $"{nameof(SpecificIntent)}");
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
--- src/trunk/Code/NoFuture/Rand/Law/Criminal/AgainstPublic/US/Terms/Vagueness.cs
+++ src/trunk/Code/NoFuture/Rand/Law/Criminal/AgainstPublic/US/Terms/Vagueness.cs
@@ -1,12 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using NoFuture.Rand.Law.Criminal.AgainstPublic.US.Elements;
 
 namespace NoFuture.Rand.Law.Criminal.AgainstPublic.US.Terms
 {
-    class Vagueness
+    /// <summary>
+    /// A means by which a <see cref="DisorderlyConduct"/> is found to be unconstitutional
+    /// because its ambiguity can lead to uneven application of criminal law
+    /// </summary>
+    public class Vagueness : TermCategory
     {
+        protected override string CategoryName => "vague";
     }
 }
