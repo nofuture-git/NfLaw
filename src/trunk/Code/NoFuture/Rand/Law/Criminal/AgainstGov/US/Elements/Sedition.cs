79e0021eaf4339afbb1df93b586291acbdc03906
NoFuture <brian.starrett@gmail.com>
2019-03-06 13:34:34 -0600

    added Sedition type in AgainstGov namespace

---
+++ src/trunk/Code/NoFuture/Rand/Law/Criminal/AgainstGov/US/Elements/Sedition.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using NoFuture.Rand.Law.Criminal.US;
+using NoFuture.Rand.Law.Criminal.US.Elements;
+using NoFuture.Rand.Law.Criminal.US.Elements.Act;
+using NoFuture.Rand.Law.Criminal.US.Elements.Intent;
+using NoFuture.Rand.Law.Criminal.US.Elements.Intent.ComLaw;
+using NoFuture.Rand.Law.Criminal.US.Elements.Intent.PenalCode;
+
+namespace NoFuture.Rand.Law.Criminal.AgainstGov.US.Elements
+{
+    /// <summary>
+    /// the incitement of insurrection or revolution by seditious speech or
+    /// writings, as such, is subject to the restrictions of the First Amendment
+    /// </summary>
+    public class Sedition : CriminalBase, IActusReus, IAssault
+    {
+        /// <summary>
+        /// either advocating, aiding, teaching, organizing or printing, publishing,
+        /// or circulating written matter that advocates, aids, or
+        /// teaches [...] by force or violence
+        /// </summary>
+        public Predicate<ILegalPerson> IsByThreatOfViolence { get; set; } = lp => false;
+
+        /// <summary>
+        /// the overthrow of the US government or any state, district, or territory
+        /// </summary>
+        public Predicate<ILegalPerson> IsToOverthrowGovernment { get; set; } = lp => false;
+
+        /// <summary>
+        /// specific intent or purposely to print, publish, or circulate
+        /// written matter that advocates, aids, or teaches the violent
+        /// government overthrow
+        /// </summary>
+        public Predicate<ILegalPerson> IsInWrittenForm { get; set; } = lp => false;
+
+        public override bool IsValid(params ILegalPerson[] persons)
+        {
+            var defendant = GetDefendant(persons);
+            if (defendant == null)
+                return false;
+
+            if (!IsByThreatOfViolence(defendant))
+            {
+                AddReasonEntry($"defendant, {defendant.Name}, {nameof(IsByThreatOfViolence)} is false");
+                return false;
+            }
+
+            if (!IsToOverthrowGovernment(defendant))
+            {
+                AddReasonEntry($"defendant, {defendant.Name}, {nameof(IsToOverthrowGovernment)} is false");
+                return false;
+            }
+
+            return true;
+        }
+
+        public bool CompareTo(IMensRea criminalIntent, params ILegalPerson[] persons)
+        {
+            var defendant = GetDefendant(persons);
+            if (defendant == null)
+                return false;
+            bool isValid = false;
+
+            //Yates v. U.S., 354 U.S. 298 (1957), different intent based on which predicate
+            if (IsInWrittenForm(defendant))
+            {
+                isValid = criminalIntent is Purposely || criminalIntent is SpecificIntent;
+
+                if (!isValid)
+                {
+                    AddReasonEntry($"{nameof(Sedition)}, when {nameof(IsInWrittenForm)} is true," +
+                                   $" requires intent of {nameof(Purposely)} or {nameof(SpecificIntent)}");
+                    return false;
+                }
+
+                return true;
+            }
+
+            isValid = criminalIntent is Knowingly || criminalIntent is GeneralIntent;
+
+            if (!isValid)
+            {
+                AddReasonEntry($"{nameof(Treason)}, when {nameof(IsInWrittenForm)} is false," +
+                               $" requires intent of {nameof(Knowingly)} or {nameof(GeneralIntent)}");
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
--- src/trunk/Code/NoFuture/Rand/Law/Criminal/AgainstGov/US/Elements/Treason.cs
+++ src/trunk/Code/NoFuture/Rand/Law/Criminal/AgainstGov/US/Elements/Treason.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using NoFuture.Rand.Law.Criminal.US;
+using NoFuture.Rand.Law.Criminal.US.Elements;
 using NoFuture.Rand.Law.Criminal.US.Elements.Act;
 using NoFuture.Rand.Law.Criminal.US.Elements.Intent;
 using NoFuture.Rand.Law.Criminal.US.Elements.Intent.ComLaw;
@@ -11,12 +12,12 @@ namespace NoFuture.Rand.Law.Criminal.AgainstGov.US.Elements
     /// <summary>
     /// Article III Section 3 of the US Constitution
     /// </summary>
-    public class Treason : CriminalBase, ICapitalOffense
+    public class Treason : CriminalBase, ICapitalOffense, IBattery
     {
         /// <summary>
-        /// Active belligerent or enemy-combative 
+        /// Active belligerent or enemy-combative [levying War against them]
         /// </summary>
-        public Predicate<ILegalPerson> IsLevyingWar { get; set; } = lp => false;
+        public Predicate<ILegalPerson> IsByViolence { get; set; } = lp => false;
 
         /// <summary>
         /// Joining the enemy 
@@ -39,12 +40,12 @@ namespace NoFuture.Rand.Law.Criminal.AgainstGov.US.Elements
             if (defendant == null)
                 return false;
 
-            var levy = IsLevyingWar(defendant);
+            var levy = IsByViolence(defendant);
             var adhere = IsAdheringToEnemy(defendant);
             var aid = IsGivingAidComfort(defendant);
             if (new[] {levy, adhere, aid}.All(p => p == false))
             {
-                AddReasonEntry($"defendant, {defendant.Name}, {nameof(IsLevyingWar)}, " +
+                AddReasonEntry($"defendant, {defendant.Name}, {nameof(IsByViolence)}, " +
                                $"{nameof(IsAdheringToEnemy)}, {nameof(IsGivingAidComfort)} are all false");
                 return false;
             }
@@ -74,13 +75,13 @@ namespace NoFuture.Rand.Law.Criminal.AgainstGov.US.Elements
             bool isValid = false;
 
             //Cramer v. U.S., 1945, different intent based on which predicate
-            if (IsLevyingWar(defendant))
+            if (IsByViolence(defendant))
             {
                 isValid = criminalIntent is Purposely || criminalIntent is SpecificIntent;
 
                 if (!isValid)
                 {
-                    AddReasonEntry($"{nameof(Treason)}, when {nameof(IsLevyingWar)} is true," +
+                    AddReasonEntry($"{nameof(Treason)}, when {nameof(IsByViolence)} is true," +
                                    $" requires intent of {nameof(Purposely)} or {nameof(SpecificIntent)}");
                     return false;
                 }
@@ -92,7 +93,7 @@ namespace NoFuture.Rand.Law.Criminal.AgainstGov.US.Elements
 
             if (!isValid)
             {
-                AddReasonEntry($"{nameof(Treason)}, when {nameof(IsLevyingWar)} is false," +
+                AddReasonEntry($"{nameof(Treason)}, when {nameof(IsByViolence)} is false," +
                                $" requires intent of {nameof(Knowingly)} or {nameof(GeneralIntent)}");
                 return false;
             }
