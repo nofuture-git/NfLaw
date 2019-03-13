c0aaf6bf5a067c0d81b33aea1d6249b0b0df7651
NoFuture <brian.starrett@gmail.com>
2019-03-13 15:50:50 -0500

    more work towards the movement of various concepts from Nf.Rand.Law.Criminal to root Nf.Rand.Law

--- src/trunk/Code/NoFuture/Rand/Law/Criminal/US/Elements/Harm.cs
+++ src/trunk/Code/NoFuture/Rand/Law/Criminal/US/Elements/Harm.cs
@@ -1,12 +1,50 @@
 ﻿using System;
+using System.Linq;
 
 namespace NoFuture.Rand.Law.Criminal.US.Elements
 {
+    /// <summary>
+    /// The legal idea of some kind of loss suffered
+    /// </summary>
+    /// <remarks>https://en.wikipedia.org/wiki/Harm</remarks>
     public class Harm: CriminalBase, IElement
     {
+        protected Func<ILegalPerson[], ILegalPerson> GetSubjectPerson { get; set; }
+
+        public Harm(Func<ILegalPerson[], ILegalPerson> getSubjectPerson)
+        {
+            GetSubjectPerson = getSubjectPerson;
+        }
+
+        public Predicate<ILegalPerson> IsPain { get; set; } = lp => false;
+
+        public Predicate<ILegalPerson> IsDeath { get; set; } = lp => false;
+
+        public Predicate<ILegalPerson> IsDisability { get; set; } = lp => false;
+
+        public Predicate<ILegalPerson> IsOfFreedomLost { get; set; } = lp => false;
+
+        public Predicate<ILegalPerson> IsOfPleasureLost { get; set; } = lp => false;
+
         public override bool IsValid(params ILegalPerson[] persons)
         {
-            throw new NotImplementedException();
+            var person = GetSubjectPerson(persons);
+            if (person == null)
+                return false;
+
+            var pain = IsPain(person);
+            var death = IsDeath(person);
+            var disability = IsDisability(person);
+            var freedom = IsOfFreedomLost(person);
+            var pleasure = IsOfPleasureLost(person);
+
+            if (new[] {pain, death, disability, freedom, pleasure}.All(p => p == false))
+            {
+                AddReasonEntry($"person, {person.Name}, {nameof(IsPain)}, {nameof(IsDeath)}, {nameof(IsDisability)}" +
+                               $", {nameof(IsOfFreedomLost)}, {nameof(IsOfPleasureLost)} are all false");
+            }
+
+            return true;
         }
     }
 }
---
+++ src/trunk/Code/NoFuture/Rand/Law/UnoHomine.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace NoFuture.Rand.Law
+{
+    public abstract class UnoHomine : LegalConcept
+    {
+        protected Func<ILegalPerson[], ILegalPerson> GetSubjectPerson { get; set; }
+        protected UnoHomine(Func<ILegalPerson[], ILegalPerson> getSubjectPerson)
+        {
+            GetSubjectPerson = getSubjectPerson
+        }
+    }
+}
