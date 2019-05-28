5d4f3609731606a2e2d42caf4feacf813f5b875d
NoFuture <brian.starrett@gmail.com>
2019-05-28 10:56:11 -0500

    Nf.Rand.Law tort, added cases & concepts regarding reasonable care

---
+++ src/trunk/Code/NoFuture/Rand/Law/Tort/US/Elements/ReasonableCare/OfPhysicalDeficiency.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using NoFuture.Rand.Law.US;
+
+namespace NoFuture.Rand.Law.Tort.US.Elements.ReasonableCare
+{
+    /// <inheritdoc />
+    /// <summary>
+    /// In order to meet the standard of ordinary duty under the law, those
+    /// with uncommon physical limitations must exercise
+    /// more diligence (e.g. blind, deaf, lame, feeble, etc.).
+    /// </summary>
+    public class OfPhysicalDeficiency : ReasonableCareBase
+    {
+        public OfPhysicalDeficiency(Func<ILegalPerson[], ILegalPerson> getSubjectPerson)
+        {
+            GetSubjectPerson = getSubjectPerson;
+        }
+
+        public OfPhysicalDeficiency() : this(ExtensionMethods.Tortfeasor) { }
+
+        /// <summary>
+        /// E.g. a cane, wheelchair, seeing-eye dog, etc.
+        /// </summary>
+        public Predicate<ILegalPerson> IsUsingCompensatoryDevice { get; set; } = lp => false;
+
+        public Predicate<ILegalPerson> IsAfflictedWith { get; set; } = lp => false;
+
+        public override bool IsValid(params ILegalPerson[] persons)
+        {
+            var subjPerson = GetSubjectPerson(persons);
+
+            if (subjPerson == null)
+                return false;
+            var title = subjPerson.GetLegalPersonTypeName();
+
+            var isAct = base.IsValid(persons);
+
+            if (!IsAfflictedWith(subjPerson))
+            {
+                AddReasonEntry($"{title} {subjPerson.Name}, {nameof(IsAfflictedWith)} is false");
+                return isAct;
+            }
+
+            if (!IsUsingCompensatoryDevice(subjPerson))
+            {
+                AddReasonEntry($"{title} {subjPerson.Name}, {nameof(IsUsingCompensatoryDevice)} is false");
+                return false;
+            }
+
+            return isAct;
+        }
+    }
+}
---
+++ src/trunk/Code/NoFuture/Rand/Law/Tort/US/Elements/ReasonableCare/ReasonableCareBase.cs
@@ -0,0 +1,24 @@
+﻿using NoFuture.Rand.Law.US;
+
+namespace NoFuture.Rand.Law.Tort.US.Elements.ReasonableCare
+{
+    /// <summary>
+    /// Abstract idea or caution of the hypothetical reasonable person
+    /// (i.e. a person or ordinary intelligence and prudence).
+    /// </summary>
+    /// <remarks>
+    /// <![CDATA[
+    /// The law takes no account of the infinite varieties of
+    /// temperament, intellect, and education which make the
+    /// internal character of a given act so different in
+    /// different men.
+    /// When a man has a distinct defect of such a nature that
+    /// all can recognize it as making certain precautions
+    /// impossible, he will not be held answerable for not taking them.
+    /// OLIVER WENDELL HOLMES, JR., THE COMMON LAW 107-09 (1881)
+    /// ]]>
+    /// </remarks>
+    public abstract class ReasonableCareBase : Act
+    {
+    }
+}
