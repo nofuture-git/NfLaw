b982834e0ae31354babb8a44cd5d033a02a5a69e
NoFuture <brian.starrett@gmail.com>
2019-05-29 14:13:59 -0500

    Nf.Rand.Law.Tort, added another kind of ReasonableCare

---
+++ src/trunk/Code/NoFuture/Rand/Law/Tort/US/Elements/ReasonableCare/OfMentalCapacity.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NoFuture.Rand.Law.US;
+
+namespace NoFuture.Rand.Law.Tort.US.Elements.ReasonableCare
+{
+    public class OfMentalCapacity : ReasonableCareBase
+    {
+        public OfMentalCapacity(Func<ILegalPerson[], ILegalPerson> getSubjectPerson) : base(getSubjectPerson)
+        {
+        }
+
+        public Predicate<ILegalPerson> IsMentallyIncapacitated { get; set; } = lp => false;
+
+        public Predicate<ILegalPerson> IsIncapacityForeseeable { get; set; } = lp => false;
+
+        public Predicate<ILegalPerson> IsExercisedMentalCare { get; set; } = lp => false;
+
+        public override bool IsValid(params ILegalPerson[] persons)
+        {
+            var subj = GetSubjectPerson(persons);
+            if (subj == null)
+                return false;
+
+            var title = subj.GetLegalPersonTypeName();
+
+            if (!IsMentallyIncapacitated(subj))
+            {
+                AddReasonEntry($"{title} {subj.Name}, {nameof(IsMentallyIncapacitated)} is false");
+                return false;
+            }
+
+            if (!IsIncapacityForeseeable(subj))
+            {
+                AddReasonEntry($"{title} {subj.Name}, {nameof(IsIncapacityForeseeable)} is false");
+                return true;
+            }
+
+            if (!IsExercisedMentalCare(subj))
+            {
+                AddReasonEntry($"{title} {subj.Name}, {nameof(IsExercisedMentalCare)} is false");
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
