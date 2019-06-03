93e5d46d979086706e2dba51846c1078bb577b8d
NoFuture <brian.starrett@gmail.com>
2019-06-03 15:04:35 -0500

    Nf.Rand.Law.Tort, more predicates related to NegligenceByStatute

--- src/trunk/Code/NoFuture/Rand/Law/Tort/US/Elements/NegligenceByStatute.cs
+++ src/trunk/Code/NoFuture/Rand/Law/Tort/US/Elements/NegligenceByStatute.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using NoFuture.Rand.Law.US;
 
 namespace NoFuture.Rand.Law.Tort.US.Elements
@@ -17,6 +13,13 @@ namespace NoFuture.Rand.Law.Tort.US.Elements
         }
 
         /// <summary>
+        /// The Restatement (Second) of Torts advises that using a statue to prove negligence
+        /// then all the parties in the suit must be members of the group or class of people
+        /// for which the statue intended to protect.
+        /// </summary>
+        public Predicate<ILegalPerson> IsGroupMemberOfStatuesProtection { get; set; } = lp => true;
+
+        /// <summary>
         /// Where the objective tests are directly given by Legislature
         /// </summary>
         public Predicate<ILegalPerson> IsObeyStatute { get; set; } = lp => false;
@@ -26,12 +29,42 @@ namespace NoFuture.Rand.Law.Tort.US.Elements
         /// </summary>
         public Predicate<ILegalPerson> IsObedienceAddToDanger { get; set; } = lp => false;
 
+        /// <summary>
+        /// <![CDATA[
+        /// while compliance with a statute may constitute some evidence of due care, it does
+        /// not preclude a finding of negligence.” 539 N.Y.S.2d 922, 924 (1989),
+        /// aff’d, 552 N.E.2d 162 (1990).
+        /// ]]>
+        /// </summary>
+        public Predicate<ILegalPerson> IsObedienceSufficient { get; set; } = lp => true;
+
+        /// <summary>
+        /// Idea that private parties can sue based on negligence of a
+        /// statute - Legislature may intend to directly avoid this.
+        /// </summary>
+        public Predicate<ILegalPerson> IsDisobedienceCauseForAction { get; set; } = lp => false;
+
         public override bool IsValid(params ILegalPerson[] persons)
         {
             var subj = GetSubjectPerson(persons);
             if (subj == null)
                 return false;
             var title = subj.GetLegalPersonTypeName();
+
+            foreach (var p in persons)
+            {
+                if (!IsGroupMemberOfStatuesProtection(p))
+                {
+                    AddReasonEntry($"{title} {p.Name}, {nameof(IsGroupMemberOfStatuesProtection)} is false");
+                    return false;
+                }
+
+                if (!IsDisobedienceCauseForAction(p))
+                {
+                    AddReasonEntry($"{title} {p.Name}, {nameof(IsDisobedienceCauseForAction)} is false");
+                    return false;
+                }
+            }
 
             var obey = IsObeyStatute(subj);
             var disobeyOk = IsObedienceAddToDanger(subj);
@@ -43,8 +76,11 @@ namespace NoFuture.Rand.Law.Tort.US.Elements
                 return false;
             }
 
-            if (obey)
+            var obeyEnough = IsObedienceSufficient(subj);
+
+            if (obey && obeyEnough)
             {
+                AddReasonEntry($"{title} {subj.Name}, {nameof(IsObedienceSufficient)} is true");
                 AddReasonEntry($"{title} {subj.Name}, {nameof(IsObeyStatute)} is true");
                 return false;
             }
