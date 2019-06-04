647413888d61ae9d35c43eba4414d12e7ec6f3a9
NoFuture <brian.starrett@gmail.com>
2019-06-04 15:13:21 -0500

    Nf.Rand.Law.Tort, added legal concepts and unit test of ContributoryNegligence and ComparativeNegligence

---
+++ src/trunk/Code/NoFuture/Rand/Law/Tort/US/Elements/ComparativeNegligence.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace NoFuture.Rand.Law.Tort.US.Elements
+{
+    /// <summary>
+    /// allocated liability &quot;in direct proportion to the extent of
+    /// the parties&apos; causal responsibility (a.k.a. fault).&quot;
+    /// </summary>
+    public class ComparativeNegligence<T> : ContributoryNegligence<T> where T : IRankable
+    {
+        public ComparativeNegligence(Func<ILegalPerson[], ILegalPerson> getSubjectPerson) : base(getSubjectPerson)
+        {
+        }
+
+        public override bool IsValid(params ILegalPerson[] persons)
+        {
+            var subj = GetSubjectPerson(persons);
+            if (subj == null)
+                return false;
+
+            var subjContribution = GetContributionWithReasonEntry(subj);
+            var result = subjContribution > 0;
+            
+            foreach (var person in persons.Where(p => !ReferenceEquals(subj, p)))
+            {
+                var pContribution = GetContributionWithReasonEntry(person);
+
+                if (pContribution > subjContribution)
+                    result = false;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ src/trunk/Code/NoFuture/Rand/Law/Tort/US/Elements/ContributoryNegligence.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Linq;
+using NoFuture.Rand.Law.US;
+using NoFuture.Rand.Law.US.Persons;
+
+namespace NoFuture.Rand.Law.Tort.US.Elements
+{
+    /// <summary>
+    /// is founded upon the [...] impossibility of assigning all effects to their respective causes
+    /// </summary>
+    /// <remarks>
+    /// source: CHARLES FISK BEACH, JR., THE LAW OF CONTRIBUTORY NEGLIGENCE 11-13 (1885).
+    /// </remarks>
+    public class ContributoryNegligence<T> : UnoHomine where T : IRankable
+    {
+        public ContributoryNegligence(Func<ILegalPerson[], ILegalPerson> getSubjectPerson) : base(getSubjectPerson)
+        {
+        }
+
+        public Func<ILegalPerson, T> GetContribution { get; set; } = lp => default(T);
+
+        public override bool IsValid(params ILegalPerson[] persons)
+        {
+            var subj = GetSubjectPerson(persons);
+            if (subj == null)
+                return false;
+
+            var subjContribution = GetContributionWithReasonEntry(subj);
+
+            if (subjContribution <= 0)
+            {
+                AddReasonEntry(
+                    $"{subj.GetLegalPersonTypeName()} {subj.Name}, {nameof(GetContribution)} is zero or less");
+                return false;
+            }
+
+            foreach (var person in persons.Where(p => !ReferenceEquals(subj, p) && p is IPlaintiff))
+            {
+                var pContribution = GetContributionWithReasonEntry(person);
+
+                if (pContribution > 0)
+                {
+                    AddReasonEntry($"{person.GetLegalPersonTypeName()} {person.Name}, " +
+                                   $"{nameof(GetContribution)} is greater-than-zero");
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        protected internal virtual int GetContributionWithReasonEntry(ILegalPerson person)
+        {
+            if (person == null)
+                return 0;
+
+            var contribution = GetContribution(person)?.GetRank() ?? 0;
+
+            AddReasonEntry($"{person.GetLegalPersonTypeName()} {person.Name}, " +
+                           $"{nameof(GetContribution)} {nameof(IRankable.GetRank)} " +
+                           $"return value of {contribution}");
+
+            return contribution;
+        }
+    }
+}
