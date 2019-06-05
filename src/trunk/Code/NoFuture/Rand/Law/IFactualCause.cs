5bdf65828fbc00d849603b7d04acefb82c5d347f
NoFuture <brian.starrett@gmail.com>
2019-06-05 13:31:14 -0500

    Nf.Rand.Law, file missing from last commit

---
+++ src/trunk/Code/NoFuture/Rand/Law/IFactualCause.cs
@@ -0,0 +1,16 @@
+﻿namespace NoFuture.Rand.Law
+{
+    /// <summary>
+    /// The direct antecedent which caused the harm\damage - the harm\damage which exist only because of it.
+    /// </summary>
+    /// <remarks>
+    /// <![CDATA[
+    /// taken in reverse to absurdity, "he pushed him off the cliff, but he
+    /// did not kill him... he was alive after being pushed ... he
+    /// died because of the rocks at the bottom..."
+    /// ]]>
+    /// </remarks>
+    public interface IFactualCause : ILegalConcept
+    {
+    }
+}
