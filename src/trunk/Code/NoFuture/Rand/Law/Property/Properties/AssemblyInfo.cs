c9da0a21bf95af6b308046271f80d47d408ecfc5
NoFuture <brian.starrett@gmail.com>
2019-06-27 17:35:50 -0500

    added new project for Law.Property, moved the types in Nf.Rand.Law.US.Property to this new project and adjusted refs as needed, also added sister test project

---
+++ src/trunk/Code/NoFuture/Rand/Law/Property/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("NoFuture.Rand.Law.Property")]
+[assembly: AssemblyDescription("Contains types to represent legal concepts of property law")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NoFuture.Rand.Law.Property")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("25a94905-7e58-463c-bed8-d3004135cfcc")]
+[assembly: InternalsVisibleTo("NoFuture.Rand.Law.Property.Tests")]
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.0.0.0")]
+//[assembly: AssemblyFileVersion("1.0.0.0")]
