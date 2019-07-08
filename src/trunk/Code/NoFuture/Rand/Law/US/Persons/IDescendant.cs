ee06b8cd70819bce1e58a46ddeff60a34b956d62
NoFuture <brian.starrett@gmail.com>
2019-07-08 10:53:43 -0500

    Nf.Rand.Law.Property, legal concept of artifacts

---
+++ src/trunk/Code/NoFuture/Rand/Law/US/Persons/IDescendant.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NoFuture.Rand.Law.US.Persons
+{
+    /// <summary>
+    /// a relational position of one person to another through hereditary means 
+    /// </summary>
+    public interface IDescendant : ILegalPerson
+    {
+        Predicate<ILegalPerson> IsDescendantOf { get; set; }
+    }
+}
