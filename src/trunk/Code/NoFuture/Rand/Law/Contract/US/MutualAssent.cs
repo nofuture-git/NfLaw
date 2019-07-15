30ec10888ebd96a5c145d2559be02457ba0ca2ca
NoFuture <brian.starrett@gmail.com>
2019-07-15 15:31:31 -0500

    Nf.Rand.Law.Contract, again, updated MutualAssent namespace to match

--- src/trunk/Code/NoFuture/Rand/Law/Contract/US/MutualAssent.cs
+++ src/trunk/Code/NoFuture/Rand/Law/Contract/US/MutualAssent.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using NoFuture.Rand.Law.US;
 
-namespace NoFuture.Rand.Law.US
+namespace NoFuture.Rand.Law.Contract.US
 {
     public class MutualAssent : LegalConcept, IAssentTerms
     {
