9f6a7d062dd8bbc93a1c9cd55f3dfbcd59b3c3ea
NoFuture <brian.starrett@gmail.com>
2019-07-25 09:57:34 -0500

    Nf.Rand.Law, added type for legal person title of "heir", added hotkeys found regarding hebrew lang

---
+++ src/trunk/Code/NoFuture/Rand/Law/US/Persons/IHeir.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NoFuture.Rand.Law.US.Persons
+{
+    /// <summary>
+    /// an individual who is legally entitled, by statute or by will, to inherit a decedent&apos;s estate
+    /// </summary>
+    public interface IHeir : ILegalPerson
+    {
+        Predicate<ILegalPerson> IsHeirOf { get; set; }
+    }
+}
