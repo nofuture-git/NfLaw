c13a5f67b4c8fc88a3ebaa89fd1db51dbdbe9ede
NoFuture <brian.starrett@gmail.com>
2019-07-26 15:04:43 -0500

    Nf.Rand.Law.Property, added another cmdlet to Property/PropertyInterestModule.psm1 to prompt a user in a Q\A style

--- src/trunk/Code/NoFuture/Rand/Law/Property/PropertyInterestModule.psm1
+++ src/trunk/Code/NoFuture/Rand/Law/Property/PropertyInterestModule.psm1
@@ -20,8 +20,7 @@ $falsePredicate = [NoFuture.Rand.Law.US.ExtensionMethods]::FalsePredicateFx
     
     .DESCRIPTION
     Gets a new instance of PropertyInterestFactory for 
-    some RealProperty named PropertyName for some 
-    person named PersonName
+    some RealProperty named PropertyName
     
     .PARAMETER PropertyName
     The name of the RealProperty
@@ -51,5 +50,71 @@ function Get-PropertyInterestFactory
     {
         $property = New-Object NoFuture.Rand.Law.Property.US.FormsOf.InTerra.RealProperty($PropertyName)
         return New-Object NoFuture.Rand.Law.Property.US.FormsOf.InTerra.PropertyInterestFactory($property, [NoFuture.Rand.Law.US.ExtensionMethods]::FirstOne)
+    }
+}
+
+<#
+    .SYNOPSIS
+    Prompts user with questions to determine the property interest
+    
+    .DESCRIPTION
+    Performs a REPL set of question to determine the property interest
+    
+    .PARAMETER PropertyName
+    The name of the RealProperty
+
+    .PARAMETER PersonName
+    The name of the LegalPerson
+
+    .EXAMPLE
+    PS C:\> 
+
+    .OUTPUTS
+    NoFuture.Rand.Law.Property.US.FormsOf.InTerra.Interests.ILandPropertyInterest
+    
+#>
+function Invoke-PropertyInterestPrompt
+{
+    [CmdletBinding()]
+    Param
+    (
+        [Parameter(Mandatory=$true,position=0)]
+        [string] $PropertyName,
+        [Parameter(Mandatory=$true,position=1)]
+        [string] $PersonName
+
+    )
+    Process
+    {
+        $person = New-Object NoFuture.Rand.Law.LegalPerson($PersonName)
+
+        $factory = Get-PropertyInterestFactory -PropertyName $PropertyName
+
+        while($factory.IsEnd -eq $false){
+
+            $questionMethod = $factory | Get-Member | ? {$_.Name -like "Is*" -and $_.MemberType -eq "Method"}
+
+            $questionMethodName = $questionMethod.Name
+
+            $questionPrompt = [NoFuture.Util.Core.NfString]::TransformCaseToSeparator($questionMethodName, [char]" ")
+
+            $answer = Read-Host -Prompt ("{0}? (Y/N) " -f $questionPrompt) 
+
+            if([string]::Equals($answer, "y", [System.StringComparison]::OrdinalIgnoreCase)){
+                $predicate = $truePredicate
+            }
+            else{
+                $predicate = $falsePredicate
+            }
+
+            $factory = $factory.$questionMethodName($predicate, $person)
+        }
+
+        $interest = $factory.GetValue()
+
+        Write-Host [NoFuture.Util.Core.NfString]::TransformCaseToSeparator($interest.GetType().Name, [char]" ")
+
+        return $interest
+
     }
 }
