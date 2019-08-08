8ce828df1ac9521d0d8fccc685c6b8f6114e9ebc
nofuture-git <brian.starrett@gmail.com>
2019-08-08 14:35:06 -0500

    Nf.Rand.Law.Property, renamed InTerra.Interest to InTerra.Sequential

---
+++ src/trunk/Code/NoFuture/Rand/Law/US/Persons/ICotenant.cs
@@ -0,0 +1,9 @@
+﻿namespace NoFuture.Rand.Law.US.Persons
+{
+    /// <summary>
+    /// One who has shared ownership in some legal property
+    /// </summary>
+    public interface ICotenant : ILegalPerson
+    {
+    }
+}
