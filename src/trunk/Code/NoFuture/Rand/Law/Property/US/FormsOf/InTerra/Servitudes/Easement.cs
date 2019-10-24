c9e6a387a64fc9c6a70ef1599b7ce8fbdd57b0ff
NoFuture <brian.starrett@gmail.com>
2019-10-24 16:33:34 -0500

    more reading notes in Nf.Rand.Law.Property

--- src/trunk/Code/NoFuture/Rand/Law/Property/US/FormsOf/InTerra/Servitudes/Easement.cs
+++ src/trunk/Code/NoFuture/Rand/Law/Property/US/FormsOf/InTerra/Servitudes/Easement.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using NoFuture.Rand.Law.Attributes;
 
 namespace NoFuture.Rand.Law.Property.US.FormsOf.InTerra.Servitudes
 {
     /// <summary>
     /// Servitude regarding the use of another&apos;s land
     /// </summary>
+    [Aka("frontage", "cartway", "accessway")]
     public class Easement : LandPropertyInterestBase
     {
         public override bool IsValid(params ILegalPerson[] persons)
