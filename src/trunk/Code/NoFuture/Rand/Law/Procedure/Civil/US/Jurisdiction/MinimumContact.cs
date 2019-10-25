766a36ee6dcfecfe6b78f9f4a0f1a6a8af5aa583
NoFuture <brian.starrett@gmail.com>
2019-10-25 12:06:25 -0500

    fleshed out the content of MinimumContact in Nf.Rand.Law.Procedure.Civil and added unit test thereof

--- src/trunk/Code/NoFuture/Rand/Law/Procedure/Civil/US/Jurisdiction/MinimumContact.cs
+++ src/trunk/Code/NoFuture/Rand/Law/Procedure/Civil/US/Jurisdiction/MinimumContact.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using NoFuture.Rand.Law.Attributes;
 using NoFuture.Rand.Law.US;
 
 namespace NoFuture.Rand.Law.Procedure.Civil.US.Jurisdiction
@@ -22,9 +19,57 @@ namespace NoFuture.Rand.Law.Procedure.Civil.US.Jurisdiction
 
         public MinimumContact() : base(ExtensionMethods.DefendantFx) { }
 
+        public Predicate<ILegalPerson> IsDirectContact { get; set; } = lp => false;
+
+        public Predicate<ILegalPerson> IsContractedWithResident { get; set; } = lp => false;
+
+        public Predicate<ILegalPerson> IsCommerciallyEngaged { get; set; } = lp => false;
+
+        public Predicate<ILegalPerson> IsSeekingToServe { get; set; } = lp => false;
+
+        /// <summary>
+        /// Calder v. Jones, 465 U.S. 783 (1984) - concerns the Plaintiff being effected by
+        /// the intentional tort of the defendant then the defendant can sue in their state 
+        /// </summary>
+        [Aka("Calder effects test")]
+        public Predicate<ILegalPerson> IsIntentionalTort { get; set; } = lp => false;
+
+        /// <summary>
+        /// Non-passive website interacted within the forum state
+        /// </summary>
+        /// <remarks>
+        /// <![CDATA[Zippo Manufacturing Co. v. Zippo Dot Com, Inc., 952 F. Supp. 1119 (W.D. Pa. 1997)]]>
+        /// </remarks>
+        public Predicate<ILegalPerson> IsActiveVirtualContact { get; set; } = lp => false;
+
         public override bool IsValid(params ILegalPerson[] persons)
         {
-            throw new NotImplementedException();
+            var defendant = GetSubjectPerson(persons);
+            if (defendant == null)
+                return false;
+
+            var title = defendant.GetLegalPersonTypeName();
+
+            var predicateTuple = new[]
+            {
+                Tuple.Create(IsDirectContact, nameof(IsDirectContact)),
+                Tuple.Create(IsContractedWithResident, nameof(IsContractedWithResident)),
+                Tuple.Create(IsCommerciallyEngaged, nameof(IsCommerciallyEngaged)),
+                Tuple.Create(IsSeekingToServe, nameof(IsSeekingToServe)),
+                Tuple.Create(IsIntentionalTort, nameof(IsIntentionalTort)),
+                Tuple.Create(IsActiveVirtualContact, nameof(IsActiveVirtualContact)),
+            };
+
+            foreach (var tuple in predicateTuple)
+            {
+                if (tuple.Item1(defendant))
+                {
+                    AddReasonEntry($"{title} {defendant.Name}, {tuple.Item2} is true");
+                    return true;
+                }
+            }
+
+            return false;
         }
     }
 }
