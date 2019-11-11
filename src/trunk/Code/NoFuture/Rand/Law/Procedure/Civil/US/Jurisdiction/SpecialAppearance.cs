0b5ef6f54102cc394e0b9b94197ca6eff93ccb5b
NoFuture <brian.starrett@gmail.com>
2019-11-11 13:30:38 -0600

    missing from last commit

--- src/trunk/Code/NoFuture/Rand/Law/Procedure/Civil/US/Jurisdiction/SpecialAppearance.cs
+++ src/trunk/Code/NoFuture/Rand/Law/Procedure/Civil/US/Jurisdiction/SpecialAppearance.cs
@@ -1,37 +1,15 @@
-﻿using NoFuture.Rand.Law.US;
+﻿using NoFuture.Rand.Law.Procedure.Civil.US.Pleadings;
 
-namespace NoFuture.Rand.Law.Procedure.Civil.US.Pleadings
+namespace NoFuture.Rand.Law.Procedure.Civil.US.Jurisdiction
 {
     /// <summary>
     /// A response to a <see cref="Summons"/> in which defendant
     /// objects to court&apos;s personal jurisdiction over them 
     /// </summary>
-    public class SpecialAppearance : PleadingBase
+    public class SpecialAppearance : PersonalJurisdiction
     {
-        public ILegalConcept Jurisdiction { get; set; }
-
-        public override bool IsValid(params ILegalPerson[] persons)
+        public SpecialAppearance(ICourt name) : base(name)
         {
-            if (!IsCourtAssigned())
-                return false;
-
-            var defendant = this.Defendant(persons);
-            if (defendant == null)
-                return false;
-
-            var title = defendant.GetLegalPersonTypeName();
-
-            if (Jurisdiction == null)
-            {
-                AddReasonEntry($"{title} {defendant.Name}, {nameof(Jurisdiction)} is unassigned");
-                return false;
-            }
-
-            var result = Jurisdiction.IsValid(persons);
-
-            AddReasonEntryRange(Jurisdiction.GetReasonEntries());
-
-            return result;
         }
     }
 }
