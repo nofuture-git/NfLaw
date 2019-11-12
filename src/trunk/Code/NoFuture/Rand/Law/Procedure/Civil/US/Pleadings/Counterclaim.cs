9b20153f1d794dc229fb825191c6f1400cb267ce
NoFuture <brian.starrett@gmail.com>
2019-11-12 10:10:37 -0600

    added two types for Law, Civil, Procedure concepts of cross-claim and counter-claim

---
+++ src/trunk/Code/NoFuture/Rand/Law/Procedure/Civil/US/Pleadings/Counterclaim.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace NoFuture.Rand.Law.Procedure.Civil.US.Pleadings
+{
+    /// <summary>
+    /// Complaint in opposition to some other plaintiff&apos;s
+    /// complaint both of which concern the same set of facts\circumstances.
+    /// </summary>
+    public class Counterclaim : Complaint
+    {
+        public Counterclaim(PleadingBase responseTo)
+        {
+            if(responseTo == null)
+                throw new ArgumentNullException(nameof(responseTo));
+
+            ResponseTo = responseTo;
+        }
+
+        /// <summary>
+        /// The source which makes this a counter-claim instead of just a stand-alone <see cref="Complaint"/>
+        /// </summary>
+        /// <remarks>
+        /// The underlying <see cref="CivilProcedureBase.CausesOfAction"/> should equal each other
+        /// </remarks>
+        public PleadingBase ResponseTo { get; }
+
+        public override bool IsValid(params ILegalPerson[] persons)
+        {
+            //a counter claim must arise from the same set of facts
+            if (!ResponseTo.CausesOfAction.Equals(CausesOfAction))
+            {
+                AddReasonEntry($"this {nameof(CausesOfAction)} does not equal the {nameof(ResponseTo)} {nameof(CausesOfAction)}");
+                return false;
+            }
+
+            return base.IsValid(persons);
+        }
+    }
+}
---
+++ src/trunk/Code/NoFuture/Rand/Law/Procedure/Civil/US/Pleadings/Crossclaim.cs
@@ -0,0 +1,21 @@
+﻿namespace NoFuture.Rand.Law.Procedure.Civil.US.Pleadings
+{
+    /// <summary>
+    /// Similar to <see cref="Counterclaim"/> except it concerns members
+    /// of the same adversarial side
+    /// (e.g. defendant-against-defendant, plaintiff-against-plaintiff)
+    /// </summary>
+    public class Crossclaim : Complaint
+    {
+        public Crossclaim(PleadingBase relatedTo)
+        {
+            RelatedTo = relatedTo;
+        }
+
+        /// <summary>
+        /// Unlike, <see cref="Counterclaim"/>, this does not have to
+        /// be related to the original <see cref="Complaint"/> 
+        /// </summary>
+        public PleadingBase RelatedTo { get; }
+    }
+}
