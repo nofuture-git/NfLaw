b3ec9452b6576288bc88822c788f216100c2a532
NoFuture <brian.starrett@gmail.com>
2019-11-22 11:07:55 -0600

    Nf.Rand.Law.Civil.Procedure, added implementation for the other valid form of amendment

--- src/trunk/Code/NoFuture/Rand/Law/Procedure/Civil/US/Pleadings/Amendment.cs
+++ src/trunk/Code/NoFuture/Rand/Law/Procedure/Civil/US/Pleadings/Amendment.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using NoFuture.Rand.Law.Attributes;
 using NoFuture.Rand.Law.Procedure.Civil.US.ServiceOfProcess;
 using NoFuture.Rand.Law.US;
+using NoFuture.Rand.Law.US.Persons;
 
 namespace NoFuture.Rand.Law.Procedure.Civil.US.Pleadings
 {
@@ -16,6 +19,12 @@ namespace NoFuture.Rand.Law.Procedure.Civil.US.Pleadings
         /// What the this amendment is being amended to
         /// </summary>
         public ILegalConcept LinkedTo { get; set; }
+
+        /// <summary>
+        /// This is required from court or opposing party after the <see cref="AllowedTimeSpanAfterServing"/> as ended
+        /// </summary>
+        [Aka("obtained permission")]
+        public IAssent Assent { get; set; }
 
         /// <summary>
         /// The service-of-process based on each person
@@ -35,6 +44,15 @@ namespace NoFuture.Rand.Law.Procedure.Civil.US.Pleadings
             if (!TryGetSubjectPerson(persons, out var subjectPerson))
                 return false;
 
+            return IsValidWithLeaveOfCourt(persons, subjectPerson) || IsValidAsMatterOfCourse(persons, subjectPerson);
+        }
+
+        /// <summary>
+        /// One of two ways in which an amendment is valid, depends on being
+        /// filed in some limited timespan after service of process
+        /// </summary>
+        protected internal virtual bool IsValidAsMatterOfCourse(ILegalPerson[] persons, ILegalPerson subjectPerson)
+        {
             var title = subjectPerson.GetLegalPersonTypeName();
 
             var serviceOfProcess = GetServiceOfProcess(subjectPerson);
@@ -65,9 +83,36 @@ namespace NoFuture.Rand.Law.Procedure.Civil.US.Pleadings
             return true;
         }
 
+        /// <summary>
+        /// Two of two ways in which an amendment is valid, requires the court&apos;s permission or opposition&apos;s consent
+        /// </summary>
+        /// <param name="persons"></param>
+        /// <param name="subjectPerson"></param>
+        /// <returns></returns>
+        protected internal virtual bool IsValidWithLeaveOfCourt(ILegalPerson[] persons, ILegalPerson subjectPerson)
+        {
+            if (Assent == null)
+                return false;
+
+            var courtOfficial = GetPersonsLessThisOne(persons, subjectPerson).CourtOfficial();
+
+            if (Assent.IsApprovalExpressed(courtOfficial))
+                return true;
+
+            if (!TryGetOppositionPerson(persons, subjectPerson, out var opposition))
+            {
+                return false;
+            }
+
+            return Assent.IsApprovalExpressed(opposition);
+        }
+
         protected internal bool TryGetSubjectPerson(ILegalPerson[] persons, out ILegalPerson person)
         {
             person = null;
+
+            if (persons == null || !persons.Any())
+                return false;
 
             if (LinkedTo == null)
             {
@@ -88,6 +133,56 @@ namespace NoFuture.Rand.Law.Procedure.Civil.US.Pleadings
             }
 
             return true;
+        }
+
+        protected internal bool TryGetOppositionPerson(ILegalPerson[] persons, ILegalPerson subjectPerson,
+            out ILegalPerson person)
+        {
+            person = null;
+            if (subjectPerson == null)
+                return false;
+
+            var personsLessSubj = GetPersonsLessThisOne(persons, subjectPerson);
+            if (personsLessSubj == null || !personsLessSubj.Any())
+                return false;
+
+            if (subjectPerson is IPlaintiff)
+                person = this.Defendant(personsLessSubj);
+            if (subjectPerson is IDefendant)
+                person = this.Plaintiff(personsLessSubj);
+
+            return person != null;
+        }
+
+        private IList<ILegalPerson> GetPersonsLessThisOne(ILegalPerson[] persons, ILegalPerson exceptThisGuy)
+        {
+            var outPersons = new List<ILegalPerson>();
+
+            if (persons == null || !persons.Any())
+                return outPersons;
+
+            if (exceptThisGuy == null)
+                return persons.ToList();
+
+            foreach (var person in persons)
+            {
+                if (NamesEqual(person, exceptThisGuy))
+                    continue;
+                if (ReferenceEquals(person, exceptThisGuy))
+                    continue;
+                outPersons.Add(person);
+            }
+
+            return outPersons;
+        }
+
+        private ILegalPerson GetCourtOfficialLessThisOne(ILegalPerson[] persons, ILegalPerson exceptThisGuy)
+        {
+            if (persons == null || !persons.Any())
+                return null;
+
+
+            throw new NotImplementedException();
         }
 
     }
