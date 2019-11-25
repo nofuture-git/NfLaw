ced666286109dba96f7d8c39816c5f97b705715d
NoFuture <brian.starrett@gmail.com>
2019-11-25 16:17:10 -0600

    Nf.Rand.Law.Civil.Procedure, commit prior to a method name change

--- src/trunk/Code/NoFuture/Rand/Law/Procedure/Civil/US/Discovery/Depositions.cs
+++ src/trunk/Code/NoFuture/Rand/Law/Procedure/Civil/US/Discovery/Depositions.cs
@@ -6,6 +6,14 @@ using System.Threading.Tasks;
 
 namespace NoFuture.Rand.Law.Procedure.Civil.US.Discovery
 {
+    /// <summary>
+    /// The taking of testimony from a witness under oath.
+    /// From Fed.R.Civ.P. 30(c)(1)
+    /// </summary>
+    /// <remarks>
+    /// This useful form of discovery requires that one already has
+    /// a pre-existing understanding of the case&apos;s details
+    /// </remarks>
     public class Depositions : ScopeOfDiscovery
     {
     }
