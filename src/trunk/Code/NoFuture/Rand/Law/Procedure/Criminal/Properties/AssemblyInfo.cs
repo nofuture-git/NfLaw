4c25658273283652c364334ffba6f7eaf9db3d6e
NoFuture <brian.starrett@gmail.com>
2019-12-02 16:18:17 -0600

    Nf.Rand.Law.Criminal.Procedure, init'ed new project for new text on criminal procedure along with first concepts

---
+++ src/trunk/Code/NoFuture/Rand/Law/Procedure/Criminal/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("NoFuture.Rand.Law.Procedure.Criminal")]
+[assembly: AssemblyDescription("Contains types to represent legal concepts of criminal procedure")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NoFuture.Rand.Law.Procedure.Criminal")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+[assembly: InternalsVisibleTo("NoFuture.Rand.Law.Procedure.Criminal.Tests")]
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e0f6d6a3-23fb-44ef-8247-89b6ca07c905")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.0.0.0")]
+//[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ src/trunk/Code/NoFuture/Rand/Law/Procedure/Criminal/US/Search.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using NoFuture.Rand.Core;
+using NoFuture.Rand.Law.US;
+using NoFuture.Rand.Law.US.Persons;
+
+namespace NoFuture.Rand.Law.Procedure.Criminal.US
+{
+    /// <summary>
+    /// The right of the people to be secure in their persons, houses, papers,
+    /// and effects, against unreasonable search and seizures, shall not be
+    /// violated, and no Warrants shall issue, but upon probable cause
+    /// </summary>
+    public class Search : LegalConcept, ISearch
+    {
+
+        /// <summary>
+        /// Resolves the person who conducted the search - they must be a government 
+        /// </summary>
+        public Func<ILegalPerson[], ILegalPerson> GetConductorOfSearch { get; set; } = lps => null;
+
+
+        /// <summary>
+        /// Resolve the place searched as some such-and-such name
+        /// </summary>
+        public Func<ILegalPerson, IVoca> GetSubjectOfSearch { get; set; } = lp => null;
+
+        /// <summary>
+        /// Fourth Amendment only applies to governmental actors or persons acting at the direction of the government.
+        /// </summary>
+        public Predicate<ILegalPerson> IsAgentOfTheState { get; set; } = lp => lp is IGovernment;
+
+        /// <summary>
+        /// persons acting at the direction of the government or pursuant to government interest
+        /// </summary>
+        public ISearch InstrumentOfTheState { get; set; }
+
+        public override bool IsValid(params ILegalPerson[] persons)
+        {
+            var searchedBy = GetConductorOfSearch(persons);
+            if (searchedBy == null)
+            {
+                AddReasonEntry($"{nameof(GetConductorOfSearch)} returned nothing");
+                return false;
+            }
+
+            var searchByTitle = searchedBy.GetLegalPersonTypeName();
+
+            if (!IsAgentOfTheState(searchedBy))
+            {
+                AddReasonEntry($"{searchByTitle} {searchedBy.Name}, {nameof(IsAgentOfTheState)} is false");
+                return false;
+            }
+
+            var isGovtAgent = IsAgentOfTheState(searchedBy);
+
+            if (InstrumentOfTheState != null && InstrumentOfTheState.GetConductorOfSearch == null)
+            {
+                InstrumentOfTheState.GetConductorOfSearch = GetConductorOfSearch;
+            }
+
+            if (!isGovtAgent && !(InstrumentOfTheState?.IsValid(persons) ?? true))
+            {
+                AddReasonEntry($"{searchByTitle} {searchedBy.Name}, {nameof(IsAgentOfTheState)} is false");
+                AddReasonEntryRange(InstrumentOfTheState.GetReasonEntries());
+                return false;
+            }
+
+            throw new NotImplementedException();
+        }
+
+    }
+}
