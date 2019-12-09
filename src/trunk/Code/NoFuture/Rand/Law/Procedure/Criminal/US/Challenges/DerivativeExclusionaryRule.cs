f677881504fb83dc12db06f2b3b3a868515090c9
NoFuture <brian.starrett@gmail.com>
2019-12-09 15:49:39 -0600

    Nf.Rand.Law.Criminal.Procedure, added types for concept of ExclusionaryRule.cs and moved ChallengeToProbableCause to the same folder

---
+++ src/trunk/Code/NoFuture/Rand/Law/Procedure/Criminal/US/Challenges/DerivativeExclusionaryRule.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using NoFuture.Rand.Law.Attributes;
+using NoFuture.Rand.Law.Procedure.Criminal.US.SearchReasons;
+using NoFuture.Rand.Law.US;
+
+namespace NoFuture.Rand.Law.Procedure.Criminal.US.Challenges
+{
+    /// <summary>
+    /// when the source of evidence is from some other unlawful evidence 
+    /// </summary>
+    /// <typeparam name="T">The type of the evidence</typeparam>
+    [Aka("fruit-of-poisonous-tree")]
+    public class DerivativeExclusionaryRule<T> : LegalConcept, IRankable
+    {
+        public Func<ILegalPerson, T> GetDerivativeEvidence { get; set; } = lp => default(T);
+        public Func<ILegalPerson[], ILegalPerson> GetLawEnforcement { get; set; } = lps => lps.LawEnforcement();
+
+        /// <summary>
+        /// Primary predicate that asserts if the evidence is even in scope of the derivative exclusionary rule
+        /// </summary>
+        public Predicate<T> IsDerivedFromUnlawfulSource { get; set; } = l => false;
+
+        /// <summary>
+        /// (1) The time period between the illegality and acquisition of secondary evidence concludes to a dissipation of connection
+        /// </summary>
+        public Predicate<T> IsLongTimePeriodAttenuated { get; set; } = l => false;
+
+        /// <summary>
+        /// (2) the more links in the chain the more attenuated the connection
+        /// </summary>
+        public Predicate<T> IsInterveningEventsAttenuated { get; set; } = l => false;
+
+        /// <summary>
+        /// (3) deliberate and flagrant the constitutional violation derive to greater attenuation 
+        /// </summary>
+        public Predicate<ILegalPerson> IsUnintentionalMinorAttenuated { get; set; } = l => false;
+
+        public override bool IsValid(params ILegalPerson[] persons)
+        {
+            var officer = GetLawEnforcement(persons);
+            if (officer == null)
+            {
+                AddReasonEntry($"{nameof(GetLawEnforcement)} returned nothing");
+                return false;
+            }
+
+            var officerTitle = officer.GetLegalPersonTypeName();
+            var evidence = GetDerivativeEvidence(officer);
+            if (object.Equals(evidence, null))
+            {
+                AddReasonEntry($"{officerTitle} {officer.Name}, {nameof(GetDerivativeEvidence)} returned nothing");
+                return false;
+            }
+
+            if (IsLongTimePeriodAttenuated(evidence))
+            {
+                AddReasonEntry($"{officerTitle} {officer.Name}, {nameof(IsLongTimePeriodAttenuated)} is true");
+                return false;
+            }
+
+            if (IsInterveningEventsAttenuated(evidence))
+            {
+                AddReasonEntry($"{officerTitle} {officer.Name}, {nameof(IsInterveningEventsAttenuated)} is true");
+                return false;
+            }
+
+            if (IsUnintentionalMinorAttenuated(officer))
+            {
+                AddReasonEntry($"{officerTitle} {officer.Name}, {nameof(IsUnintentionalMinorAttenuated)} is true");
+                return false;
+            }
+
+            var isDerivative = IsDerivedFromUnlawfulSource(evidence);
+            AddReasonEntry($"{officerTitle} {officer.Name}, {nameof(IsDerivedFromUnlawfulSource)} is {isDerivative}");
+            return isDerivative;
+        }
+
+        public int GetRank()
+        {
+            return new ProbableCause().GetRank() + 1;
+        }
+    }
+}
---
+++ src/trunk/Code/NoFuture/Rand/Law/Procedure/Criminal/US/Challenges/ExclusionaryRule.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using NoFuture.Rand.Law.Procedure.Criminal.US.SearchReasons;
+using NoFuture.Rand.Law.US;
+
+namespace NoFuture.Rand.Law.Procedure.Criminal.US.Challenges
+{
+    /// <summary>
+    /// requires the suppression of evidence obtained in violation of the defendant&apos;s constitutional rights
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class ExclusionaryRule<T> : LegalConcept, IRankable
+    {
+        private DerivativeExclusionaryRule<T> _derivativeExclusionary;
+
+        public Func<ILegalPerson, T> GetEvidence { get; set; } = lp => default(T);
+        public Func<ILegalPerson[], ILegalPerson> GetLawEnforcement { get; set; } = lps => lps.LawEnforcement();
+
+        public Predicate<T> IsObtainedThroughUnlawfulMeans { get; set; } = l => false;
+
+        public DerivativeExclusionaryRule<T> DerivativeExclusion
+        {
+            get => _derivativeExclusionary;
+            set
+            {
+                _derivativeExclusionary = value;
+                if (_derivativeExclusionary == null) 
+                    return;
+                _derivativeExclusionary.GetDerivativeEvidence = GetEvidence;
+                _derivativeExclusionary.GetLawEnforcement = GetLawEnforcement;
+            }
+        }
+
+        public override bool IsValid(params ILegalPerson[] persons)
+        {
+            var officer = GetLawEnforcement(persons);
+            if (officer == null)
+            {
+                AddReasonEntry($"{nameof(GetLawEnforcement)} returned nothing");
+                return false;
+            }
+
+            var officerTitle = officer.GetLegalPersonTypeName();
+            var evidence = GetEvidence(officer);
+            if (object.Equals(evidence, null))
+            {
+                AddReasonEntry($"{officerTitle} {officer.Name}, {nameof(GetEvidence)} returned nothing");
+                return false;
+            }
+
+            var isUnlawful = IsObtainedThroughUnlawfulMeans(evidence);
+            AddReasonEntry($"{officerTitle} {officer.Name}, {nameof(IsObtainedThroughUnlawfulMeans)} is {isUnlawful}");
+
+            if (isUnlawful)
+            {
+                return true;
+            }
+
+            if (DerivativeExclusion == null)
+                return false;
+
+            isUnlawful = DerivativeExclusion.IsValid(persons);
+            AddReasonEntryRange(DerivativeExclusion.GetReasonEntries());
+            return isUnlawful;
+        }
+
+        public int GetRank()
+        {
+            return new ProbableCause().GetRank() + 1;
+        }
+    }
+}
