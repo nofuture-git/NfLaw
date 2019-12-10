dae31b07a389f9396b89a43649387b73fcde9a49
NoFuture <brian.starrett@gmail.com>
2019-12-10 16:47:55 -0600

    Nf.Rand.Law.Criminal.Procedure, added new namespace for next subject of interrogations along with first concepts from text

---
+++ src/trunk/Code/NoFuture/Rand/Law/Procedure/Criminal/US/Interrogations/MirandaApproach.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using NoFuture.Rand.Law.Attributes;
+using NoFuture.Rand.Law.US;
+
+namespace NoFuture.Rand.Law.Procedure.Criminal.US.Interrogations
+{
+    /// <summary>
+    /// The familiar warnings given to a person by law enforcement prior to interrogation 
+    /// </summary>
+    /// <remarks>
+    /// questioning initiated by law enforcement officers after a person has
+    /// taken into custody or otherwise deprived of his freedom of action
+    /// in any significant way 384 U.S. at 443
+    /// </remarks>
+    [Aka("custodial interrogation")]
+    public class MirandaApproach : LegalConcept
+    {
+        public Func<ILegalPerson[], ILegalPerson> GetSuspect { get; set; } = lps => lps.Suspect();
+
+        public Predicate<ILegalPerson> IsToldRightToRemainSilent { get; set; } = lp => false;
+
+        public Predicate<ILegalPerson> IsToldRightToAttorney { get; set; } = lp => false;
+
+        [Aka("waiver of Miranda Rights")]
+        public IConsent Consent { get; set; }
+
+        /// <summary>
+        /// A test of whether a reasonable person in the
+        /// situation would have believed they are not free to leave
+        /// </summary>
+        /// <remarks>
+        /// <![CDATA[
+        /// Other factors for analysis:
+        /// * was informed as free-to-leave
+        /// * unrestrained freedom of movement
+        /// * initiated contact
+        /// * voluntarily acquiesced
+        /// * use of police strong-arm tatics
+        /// * use of police deceptive strategies
+        /// * atmosphere is police-dominated
+        /// ]]>
+        /// </remarks>
+        [Aka("custody", "police-dominated atmosphere")]
+        public Predicate<ILegalPerson> IsInCoercivePressureEnvironment { get; set; } = lp => false;
+
+        /// <summary>
+        /// encompasses conduct deliberately designed to evoke a confession, as well as
+        /// conduct that the officers should reasonably have foreseen would elicit such a response
+        /// </summary>
+        public Predicate<ILegalPerson> IsIncriminatoryQuestioning { get; set; } = lp => false;
+
+        public override bool IsValid(params ILegalPerson[] persons)
+        {
+            if (Consent != null && Consent.IsValid(persons))
+            {
+                AddReasonEntryRange(Consent?.GetReasonEntries());
+                return true;
+            }
+
+            var suspect = GetSuspect(persons);
+            if (suspect == null)
+            {
+                AddReasonEntry($"{nameof(GetSuspect)} returned nothing");
+                return false;
+            }
+
+            var suspectTitle = suspect.GetLegalPersonTypeName();
+
+            if (!IsInCoercivePressureEnvironment(suspect))
+            {
+                AddReasonEntry($"{suspectTitle} {suspect.Name}, {nameof(IsInCoercivePressureEnvironment)} is false");
+                return false;
+            }
+
+            if (!IsIncriminatoryQuestioning(suspect))
+            {
+                AddReasonEntry($"{suspectTitle} {suspect.Name}, {nameof(IsIncriminatoryQuestioning)} is false");
+                return false;
+            }
+
+            if (!IsToldRightToAttorney(suspect))
+            {
+                AddReasonEntry($"{suspectTitle} {suspect.Name}, {nameof(IsToldRightToAttorney)} is false");
+                return false;
+            }
+
+            if (!IsToldRightToRemainSilent(suspect))
+            {
+                AddReasonEntry($"{suspectTitle} {suspect.Name}, {nameof(IsToldRightToRemainSilent)} is false");
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ src/trunk/Code/NoFuture/Rand/Law/Procedure/Criminal/US/Interrogations/Voluntariness.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using NoFuture.Rand.Law.US;
+
+namespace NoFuture.Rand.Law.Procedure.Criminal.US.Interrogations
+{
+    /// <summary>
+    /// a statement must be obtained as a product of voluntary choice
+    /// </summary>
+    public class Voluntariness : LegalConcept
+    {
+        public Func<ILegalPerson[], ILegalPerson> GetSuspect { get; set; } = lps => lps.Suspect();
+
+        /// <summary>
+        /// if the police subject the suspect to coercive conduct
+        /// </summary>
+        public Predicate<ILegalPerson> IsSubjectedToCoercivePoliceConduct { get; set; } = lp => false;
+
+        /// <summary>
+        /// conduct was sufficient to overcome the will of the suspect with
+        /// respect for their particular circumstances 
+        /// </summary>
+        public Predicate<ILegalPerson> IsSufficientToOvercomeWillpower { get; set; } = lp => false;
+
+        public override bool IsValid(params ILegalPerson[] persons)
+        {
+            var suspect = GetSuspect(persons);
+            if (suspect == null)
+            {
+                AddReasonEntry($"{nameof(GetSuspect)} returned nothing");
+                return false;
+            }
+
+            var suspectTitle = suspect.GetLegalPersonTypeName();
+
+            if (IsSubjectedToCoercivePoliceConduct(suspect)
+                && IsSufficientToOvercomeWillpower(suspect))
+            {
+                AddReasonEntry($"{suspectTitle} {suspect.Name}, {nameof(IsSubjectedToCoercivePoliceConduct)} is true");
+                AddReasonEntry($"{suspectTitle} {suspect.Name}, {nameof(IsSufficientToOvercomeWillpower)} is true");
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
