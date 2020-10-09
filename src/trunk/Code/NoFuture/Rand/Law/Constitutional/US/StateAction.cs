d683bf01e462b07f3eb6842d41de0a043c0337f5
nofuture-git <brian.starrett@gmail.com>
2020-10-09 16:37:48 -0500

    added first notes on Constitutional law

--- src/trunk/Code/NoFuture/Rand/Law/Constitutional/US/StateAction.cs
+++ src/trunk/Code/NoFuture/Rand/Law/Constitutional/US/StateAction.cs
@@ -1,15 +1,73 @@
 ﻿using System;
+using NoFuture.Rand.Law.Property.US;
+using NoFuture.Rand.Law.US;
 
 namespace NoFuture.Rand.Law.Constitutional.US
 {
     /// <summary>
     /// The US Constitution doctrine of when a person is protected against incursions by government 
     /// </summary>
-    public class StateAction : LegalConcept
+    public class StateAction : PropertyConsent
     {
+        public StateAction() : base(ExtensionMethods.DefendantFx)
+        {
+        }
+
+        /// <summary>
+        /// Marsh v. Alabama 326 U.S. 501 (1946), if it looks like a town and operates like a town then it _is_ a town.
+        /// </summary>
+        public Predicate<ILegalProperty> IsPublicCommunity { get; set; } = p => false;
+
+        public Func<ILegalPerson, IAct> GetActByPerson { get; set; } = p => null;
+
+        public Predicate<IAct> IsProtectedRight { get; set; } = p => false;
+
         public override bool IsValid(params ILegalPerson[] persons)
         {
-            throw new NotImplementedException();
+            GetSubjectPerson = GetSubjectPerson ?? ExtensionMethods.DefendantFx;
+
+            var subj = GetSubjectPerson(persons);
+            var title = subj.GetLegalPersonTypeName();
+            if (subj == null)
+            {
+                AddReasonEntry($"{GetSubjectPerson} did not return any one");
+                return false;
+            }
+
+            var withConsent = !WithoutConsent(persons);
+            if (withConsent)
+            {
+                AddReasonEntry($"{nameof(WithoutConsent)} returned false - therefore consent was given");
+                return false;
+            }
+
+            var act = GetActByPerson(subj);
+
+            if (act == null)
+            {
+                AddReasonEntry($"{nameof(GetActByPerson)} returned nothing");
+                return false;
+            }
+
+            if (!act.IsValid(subj))
+            {
+                AddReasonEntryRange(act.GetReasonEntries());
+                return false;
+            }
+
+            if (!IsProtectedRight(act))
+            {
+                AddReasonEntry($"The {nameof(IsProtectedRight)} returned false for the given act of person {title} {subj.Name}");
+                return false;
+            }
+
+            if (!IsPublicCommunity(SubjectProperty))
+            {
+                AddReasonEntry($"{IsPublicCommunity} for {nameof(SubjectProperty)} {SubjectProperty.Name} is false");
+                return false;
+            }
+
+            return true;
         }
     }
 }
