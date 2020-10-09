654aa4e4ecefd49b1bd83057febaee22c6e53513
nofuture-git <brian.starrett@gmail.com>
2020-10-09 13:31:17 -0500

    start of study on US Constitutional law

---
+++ src/trunk/Code/NoFuture/Rand/Law/Constitutional/US/StateAction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace NoFuture.Rand.Law.Constitutional.US
+{
+    /// <summary>
+    /// The US Constitution doctrine of when a person is protected against incursions by government 
+    /// </summary>
+    public class StateAction : LegalConcept
+    {
+        public override bool IsValid(params ILegalPerson[] persons)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
